47de3b42a265e0d06960b2d4f44f0cbc12ce5fa3
Ashli <79345715+ashpie37@users.noreply.github.com>
2022-07-06 23:55:53 -0600

    update files

--- main.py
+++ main.py
@@ -3,107 +3,114 @@ from pygame.locals import *
 import math
 
 pygame.init()
+clock = pygame.time.Clock()
+FPS = 60
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('August')
+pygame.display.set_caption('Platform Game (Title TBH)')
 
-#load background image
-bg_img = pygame.image.load('imgs/castle.jpeg').convert()
+#colors
+color =  (122,197,205)
+#background color
+screen.fill(color)
+#loading images
+#be mindful of order I code imgs
+#bg_img = pygame.image.load('imgs/bkg2.png').convert()
+#bg_img_width = bg_img.get_width()
 
+#define game variables
+tile_size = 40 #math.ceil(SCREEN_WIDTH / bg_img_width) + 1
 
-#set framerate
-clock = pygame.time.Clock()
-FPS = 60
- 
-#define player action variables
-moving_left = False
-moving_right = False
-
-class Knight(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, scale, speed):
-        pygame.sprite.Sprite.__init__(self)
-        self.char_type = char_type
-        self.speed = speed
-        self.direction = 1
-        self.flip = False
-        img = pygame.image.load(f'{self.char_type}/Idle/0.png')
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+#grid to help place items in graphic window
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size ))
+        pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line*tile_size, SCREEN_HEIGHT))
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load('Player/Idle/0.png')
+        self.image = pygame.transform.scale(img, (40, 80))
         self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect.x = x 
+        self.rect.y = y 
+    def update(self):
+        #draw player onto screen
+        screen.blit(self.image, self. rect)
+
         
-    def move(self, moving_left, moving_right):
-        #reset movement variables
-        dx = 0
-        dy = 0
         
-        #assign movement variables if moving left or right
-        if moving_left:
-            dx = -self.speed 
-            self.flip = True
-            self.direction = -1
-        if moving_right:
-            dx = +self.speed
-            self.flip = False
-            self.direction = 1
-            
-            
-        #update rectangle position
-        self.rect.x += dx
-        self.rect.y += dy
+class World():
+    def __init__(self, data):
+        self.tile_list =[]
+    
+    #load images
+        ice_img = pygame.image.load('imgs/iceBlock.png')
+        ice_water_img = pygame.image.load('imgs/iceWaterDeep.png')
         
-            
-  
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(ice_water_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(ice_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
     def draw(self):
-        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
-        
-                  
-#where player/enemy on appears on screen 
-player = Knight('Player', 200, 200, 3, 5)
-enemy = Knight('Enemy', 200, 200, 3, 5)
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+    
 
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],  
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+]
 
+player = Player(40, SCREEN_HEIGHT - 95)
+world = World(world_data)
 
 
 run = True
 while run:
-     
-    screen.blit(bg_img, (0, 0))
-
-     
+    
+    #screen.blit(bg_img, (0, 0))
+    
+    world.draw()
+    player.update()
     clock.tick(FPS)
-    
-    player.draw()
-    enemy.draw()
-    
-    player.move(moving_left, moving_right)
-    
+    draw_grid()
     #event handler
     for event in pygame.event.get():
-        #quit game
         if event.type == pygame.QUIT:
             run = False
-            
-            
-        #keyboard presses
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                moving_left = True
-            if event.key == pygame.K_d:
-                moving_right = True 
-            if event.key == pygame.K_ESCAPE:
-                run = False
-                
-                
-        #keyboard button released 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                moving_left = False
-            if event.key == pygame.K_d:
-                moving_right = False  
-                
-    #update display            
     pygame.display.update()
+            
 pygame.quit()
