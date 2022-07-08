a618a1c189ce2edacea1d28602e722785a342e86
Ashli <79345715+ashpie37@users.noreply.github.com>
2022-07-08 01:25:08 -0600

    update files and add enemy

--- main.py
+++ main.py
@@ -4,29 +4,25 @@ import math
 
 pygame.init()
 
-#clock and frame rate
-clock = pygame.time.Clock()
-FPS = 60
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('August-The Cat Game')
 
-#colors
-color =  (143,188,143)
-
-screen.fill(color)
+#clock and frame rate
+clock = pygame.time.Clock()
+FPS = 60
 
 #define game variables
-tile_size = 40 
-
-#grid to help place items in graphic window
+tile_size = 40
+'''
+---grid to help place items in graphic window---
 def draw_grid():
     for line in range(0, 20):
-        pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size ))
-        pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line*tile_size, SCREEN_HEIGHT))
+       pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size ))
+       pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line*tile_size, SCREEN_HEIGHT))
+'''
 class Player():
     def __init__(self, x, y):
         self.images_right = []
@@ -35,7 +31,7 @@ class Player():
         self.counter = 0
         for i in range(1, 8):
             img_right = pygame.image.load(f'Fox_Player/Move/{i}.png').convert_alpha()
-            img_right = pygame.transform.scale(img_right, (50, 80))
+            img_right = pygame.transform.scale(img_right, (38, 80))
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
@@ -43,9 +39,11 @@ class Player():
         self.rect = self.image.get_rect()
         self.rect.x = x 
         self.rect.y = y 
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.vel_y = 0
         self.jump = False
-        self.direction = 1
+        self.direction = 0
         
         
     def update(self):
@@ -97,7 +95,24 @@ class Player():
         if self.vel_y > 10: 
             self.vel_y = 10
         dy += self.vel_y
+        
         #check for collision
+        for tile in world.tile_list:
+            #check in x direction
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0    
+            #check in y direction
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                #check if below the ground i.e. jumping
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                #check if above the ground i.e. falling
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+
+                
         
         #update player coordinates
         self.rect.x += dx
@@ -107,13 +122,15 @@ class Player():
             self.rect.bottom = SCREEN_HEIGHT
             dy = 0
         
-        
-        
         #draw player onto screen
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
-        
+#define colors
+color =  (143,188,143)     
+def draw_bg():
+    screen.fill(color)
+
         
 class World():
     def __init__(self, data):
@@ -141,12 +158,33 @@ class World():
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 3:
+                    skeleton = Enemy(col_count * tile_size, row_count * tile_size + 8)
+                    skeleton_group.add(skeleton)
                 col_count += 1
             row_count += 1
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
-    
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('Enemy/Idle/0.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if self.move_counter > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+        
+   
 
 world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
@@ -159,28 +197,34 @@ world_data = [
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
 [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],  
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 1],  
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
 [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
 [1, 0, 0, 0, 0, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1], 
 [1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 ]
 
-player = Player(70, SCREEN_HEIGHT - 95)
+player = Player(100, SCREEN_HEIGHT - 130)
+skeleton_group = pygame.sprite.Group()
 world = World(world_data)
 
 
 run = True
 while run:
     
+    draw_bg()
+    
     clock.tick(FPS)
     
     world.draw()
     
+    skeleton_group.update()
+    skeleton_group.draw(screen)
+    
     player.update()
     
-    draw_grid()
+    #draw_grid()
     
     #event handler
     for event in pygame.event.get():
