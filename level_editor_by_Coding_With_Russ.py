8b26749d7aa82ec2965fdb8dc91dbfc0cf64bb7c
Ashli <79345715+ashpie37@users.noreply.github.com>
2022-08-10 14:58:15 -0600

    updated files

---
+++ level_editor_by_Coding_With_Russ.py
@@ -0,0 +1,195 @@
+#level editor by Coding with Russ
+
+import pygame
+import pickle
+from os import path
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+#define colors
+color = (157, 211, 255)   
+
+#game backgrounds
+def draw_bg():
+    screen.fill(color)
+    
+#game window
+tile_size = 40
+cols = 20
+margin = 100
+SCREEN_WIDTH = tile_size * cols
+SCREEN_HEIGHT = (tile_size * cols) + margin
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+pygame.display.set_caption('Level Editor')
+
+#load images
+castle_img = pygame.image.load('imgs/castle-1.png')
+dirt_img = pygame.image.load('imgs/dirt.png')
+ground_img = pygame.image.load('imgs/ground.png')
+gem_img = pygame.image.load('imgs/gem.png')
+block_img = pygame.image.load('Enemies/Block/0.png')
+save_img = pygame.image.load('imgs/save_btn.png')
+load_img = pygame.image.load('imgs/load_btn.png')
+door_img = pygame.image.load('imgs/crown.png')
+circle_img = pygame.image.load('Enemies/Circle/0.png')
+spikes_img = pygame.image.load('imgs/spikes.png')
+water_img = pygame.image.load('imgs/water.png')
+
+#define game variables
+clicked = False
+level = 1
+
+#define colours
+white = (255, 255, 255)
+green = (144, 201, 120)
+font = pygame.font.SysFont('Futura', 14)
+
+#create empty tile list
+world_data = []
+for row in range(20):
+	r = [0] * 20
+	world_data.append(r)
+
+#create boundary
+for tile in range(0, 20):
+	world_data[19][tile] = 2
+	world_data[0][tile] = 1
+	world_data[tile][0] = 1
+	world_data[tile][19] = 1
+
+#function for outputting text onto the screen
+def draw_text(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
+def draw_grid():
+	for c in range(0, 20):
+		#vertical lines
+		pygame.draw.line(screen, white, (c * tile_size, 0), (c * tile_size, SCREEN_HEIGHT - margin))
+		#horizontal lines
+		pygame.draw.line(screen, white, (0, c * tile_size), (SCREEN_WIDTH, c * tile_size))
+
+def draw_world():
+	for row in range(16):
+		for col in range(20):
+			if world_data[row][col] > 0:
+				if world_data[row][col] == 1:
+					#dirt blocks
+					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+					screen.blit(img, (col * tile_size, row * tile_size))
+				if world_data[row][col] == 2:
+					#ground blocks
+					img = pygame.transform.scale(ground_img, (tile_size, tile_size))
+					screen.blit(img, (col * tile_size, row * tile_size))
+				if world_data[row][col] == 3:
+					#gem
+					img = pygame.transform.scale(gem_img, (tile_size // 2, tile_size // 2))
+					screen.blit(img, (col * tile_size + (tile_size // 4), row * tile_size + (tile_size // 4)))
+				if world_data[row][col] == 4:
+					#door
+					img = pygame.transform.scale(door_img, (tile_size // 2, tile_size // 2))
+					screen.blit(img, (col * tile_size + (tile_size // 4), row * tile_size + (tile_size // 4)))
+				if world_data[row][col] == 5:
+					#spikes
+					img = pygame.transform.scale(spikes_img, (tile_size // 2, tile_size // 2))
+					screen.blit(img, (col * tile_size + (tile_size // 4), row * tile_size + (tile_size // 4)))
+				if world_data[row][col] == 6:
+					#water
+					img = pygame.transform.scale(water_img, (tile_size, tile_size // 2))
+					screen.blit(img, (col * tile_size, row * tile_size + (tile_size // 2)))
+		
+
+class Button():
+	def __init__(self, x, y, image):
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self):
+		action = False
+
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				action = True
+				self.clicked = True
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button
+		screen.blit(self.image, (self.rect.x, self.rect.y))
+		return action
+
+#create load and save buttons
+save_button = Button(SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT - 80, save_img)
+load_button = Button(SCREEN_WIDTH // 2 + 50, SCREEN_HEIGHT - 80, load_img)
+
+#main game loop
+run = True
+while run:
+	clock.tick(fps)
+	draw_bg()
+
+	#load and save level
+	if save_button.draw():
+		#save level data
+		pickle_out = open(f'level{level}_data', 'wb')
+		pickle.dump(world_data, pickle_out)
+		pickle_out.close()
+	if load_button.draw():
+		#load in level data
+		if path.exists(f'level{level}_data'):
+			pickle_in = open(f'level{level}_data', 'rb')
+			world_data = pickle.load(pickle_in)
+
+	#show the grid and draw the level tiles
+	draw_grid()
+	draw_world()
+
+	#text showing current level
+	draw_text(f'Level: {level}', font, white, tile_size, SCREEN_HEIGHT - 40)
+	draw_text('Press UP or DOWN to change level', font, white, tile_size, SCREEN_HEIGHT - 60)
+
+	#event handler
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+		#mouseclicks to change tiles
+		if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+			clicked = True
+			pos = pygame.mouse.get_pos()
+			x = pos[0] // tile_size
+			y = pos[1] // tile_size
+			#check that the coordinates are within the tile area
+			if x < 20 and y < 16:
+				#update tile value
+				if pygame.mouse.get_pressed()[0] == 1:
+					world_data[y][x] += 1
+					if world_data[y][x] > 6:
+						world_data[y][x] = 0
+				elif pygame.mouse.get_pressed()[2] == 1:
+					world_data[y][x] -= 1
+					if world_data[y][x] < 0:
+						world_data[y][x] = 6
+		if event.type == pygame.MOUSEBUTTONUP:
+			clicked = False
+		#up and down key presses to change level number
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				level += 1
+			elif event.key == pygame.K_DOWN and level > 1:
+				level -= 1
+
+	#update game display window
+	pygame.display.update()
+
+pygame.quit()
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+#Platformer Pygame Tutorial by Coding with Russ
+#http://codingwithruss.com/pygame/platformer/intro.html
+
+#Sprite from Superdark
+#https://superdark.itch.io/enchanted-forest-characters
+
 #Pixel Platformer by Kenney
 #https://kenney.nl/assets/pixel-platformer
+#https://www.kenney.nl/assets/tiny-dungeon
 
 #Castle Background by Saphatthachat Sunchoote
 #https://www.dreamstime.com/pixel-art-fantasy-castle-roof-image223418688
+
+#Game Theme by xDeviruchi
+#https://xdeviruchi.itch.io/8-bit-fantasy-adventure-music-pack
 
 import pygame
 from pygame.locals import *
@@ -26,9 +36,8 @@ clock = pygame.time.Clock()
 FPS = 60
 
 #define font
-font = pygame.font.SysFont('Banhaus 93', 70)
+font = pygame.font.SysFont('Banhaus 93', 140)
 font_score = pygame.font.SysFont('Banhaus 93', 30)
-
 
 #define game variables
 tile_size = 40
@@ -49,9 +58,19 @@ start_img = pygame.image.load('imgs/start_btn.png')
 start_img = pygame.transform.scale(start_img, (270, 120))
 
 #load sounds
+pygame.mixer.music.load('Sounds/music.wav')
+pygame.mixer.music.play(-1, 0.0, 5000)
 gem_effect = pygame.mixer.Sound('Sounds/gem.wav')
+gem_effect.set_volume(0.5)
+jump_effect = pygame.mixer.Sound('Sounds/jump.wav')
+jump_effect.set_volume(0.5)
+game_over_effect = pygame.mixer.Sound('Sounds/game_over.wav')
+game_over_effect.set_volume(0.5)
+water_effect = pygame.mixer.Sound('Sounds/splash.wav')
+water_effect.set_volume(0.5)
+
 '''
----grid to help place items in graphic window---
+#grid to help place items in graphic window
 def draw_grid():
     for line in range(0, 20):
        pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (SCREEN_WIDTH, line * tile_size ))
@@ -59,7 +78,8 @@ def draw_grid():
 '''
 
 #define colors
-blue = (157, 211, 255)   
+blue = (200, 230, 252)   
+black = (0, 0, 0)
 white = (255, 255, 255)
 
 #game backgrounds
@@ -71,12 +91,12 @@ def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x,y))
 
-
 #reset function to reset level
 def reset_level(level):
     player.reset(100, SCREEN_HEIGHT - 40)
-    enemy_group.empty()
-    flag_group.empty()
+    spikes_group.empty()
+    water_group.empty()
+    door_group.empty()
 
     #load on level data and create world
     if path.exists(f'level{level}_data'):
@@ -86,7 +106,6 @@ def reset_level(level):
     return world
 
 
-    
 #buttons
 class Button():
     def __init__(self, x, y, image):
@@ -128,6 +147,7 @@ class Player():
             #keypresses
             key = pygame.key.get_pressed()
             if key[pygame.K_SPACE] and self.jump == False and self.in_air == False:
+                jump_effect.play()
                 self.vel_y = -15 
                 self.jump = True
             if key[pygame.K_SPACE] == False:
@@ -183,26 +203,30 @@ class Player():
                         self.vel_y = 0
                         self.in_air = False
                         
-            #check collision with enemies
-            if pygame.sprite.spritecollide(self, enemy_group, False):
-                game_over = -1
-            #check collision with flag
-            if pygame.sprite.spritecollide(self, flag_group, False):
+            #check collision with crown door
+            if pygame.sprite.spritecollide(self, door_group, False):
                 game_over = 1
+            #check collision with spikes
+            if pygame.sprite.spritecollide(self, spikes_group, False):
+                game_over = -1
+                game_over_effect.play()
+             #check collision with water
+            if pygame.sprite.spritecollide(self, water_group, False):
+                water_effect.play()
             
             #update player coordinates
             self.rect.x += dx
             self.rect.y += dy
             
         elif game_over == -1:
-            draw_text('GAME OVER!', font, white,(SCREEN_WIDTH // 2) - 200, SCREEN_HEIGHT // 2)
+            draw_text('GAME OVER!', font, black, 100, 300)
             self.image = self.death_image
         
         #draw player onto screen
         screen.blit(self.image, self.rect)
         #pygame.draw.rect(screen, (0, 0, 0), self.rect, 2)
         return game_over
-    
+
     def reset(self, x, y): 
         self.images_right = []
         self.images_left = []
@@ -210,7 +234,7 @@ class Player():
         self.counter = 0
         for i in range(1, 4):
             img_right = pygame.image.load(f'King/Walk/{i}.png').convert_alpha()
-            img_right = pygame.transform.scale(img_right, (38, 40))
+            img_right = pygame.transform.scale(img_right, (36, 40))
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
@@ -250,14 +274,17 @@ class World():
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                 if tile == 3:
-                    enemy = Enemy(col_count * tile_size, row_count * tile_size + 8)
-                    enemy_group.add(enemy)
-                if tile == 4:
-                    gem= Gem(col_count * tile_size, row_count * tile_size + 8)
+                    gem= Gem(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
                     gem_group.add(gem)
+                if tile == 4:
+                    door = Door(col_count * tile_size, row_count * tile_size + 2)
+                    door_group.add(door) 
                 if tile == 5:
-                    flag = Flag(col_count * tile_size, row_count * tile_size + 8)
-                    flag_group.add(flag) 
+                    spikes = Spikes(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+                    spikes_group.add(spikes)
+                if tile == 6:
+                    water = Water(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+                    water_group.add(water)
                 col_count += 1
             row_count += 1
     def draw(self):
@@ -265,14 +292,37 @@ class World():
             screen.blit(tile[0], tile[1])
             #pygame.draw.rect(screen, (0, 0, 0), tile[1], 2)
 
-class Flag(pygame.sprite.Sprite):
+
+class Door(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('imgs/flag.png')
-        self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+        img = pygame.image.load('imgs/crown.png')
+        self.image = pygame.transform.scale(img, (tile_size, (tile_size)))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+
+class Spikes(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('imgs/spikes.png')
+        self.image = pygame.transform.scale(img, (tile_size,(tile_size // 2 )))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y 
+
+
+class Water(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('imgs/water.png')
+        self.image = pygame.transform.scale(img, (tile_size, (tile_size // 2 )))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y     
+
+
 class Gem(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -280,34 +330,19 @@ class Gem(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+    
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('imgs/enemy.png')
-        self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))       
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.move_direction = 1
-        self.move_counter = 0
-        
-    def update(self):
-        self.rect.x += self.move_direction
-        self.move_counter += 1
-        if self.move_counter > 15:
-            self.move_direction *= -1
-            self.move_counter *= -1
-        
 # 16 rows, 20 columns
 #player
 player = Player(100, SCREEN_HEIGHT - 40)
-#enemy
-enemy_group = pygame.sprite.Group()
-#flag
-flag_group = pygame.sprite.Group()
+#door
+door_group = pygame.sprite.Group()
 #gem
 gem_group = pygame.sprite.Group()
+#spikes
+spikes_group = pygame.sprite.Group()
+#water
+water_group = pygame.sprite.Group()
 
 #dummy gem for showing score
 score_gem = Gem(tile_size //2, tile_size // 2)
@@ -323,16 +358,11 @@ world = World(world_data)
 restart_button = Button(340, 0, restart_img)
 start_button = Button(260, 375, start_img)
 
-
 run = True
 while run:
-    
     #draw_grid()
-
     draw_bg()
-    
     clock.tick(FPS)
-    
     if main_menu == True:
         screen.blit(castle_img,(0,0))
         screen.blit(title_img,(54,-120))
@@ -341,25 +371,26 @@ while run:
     else:
         world.draw()
         if game_over == 0:
-            enemy_group.update()
         #update score
             if pygame.sprite.spritecollide(player, gem_group, True):
                 score += 1
+                gem_effect.play()
             draw_text('X' + str(score), font_score, white, tile_size - 10, 10)
-
-        enemy_group.draw(screen)
-        flag_group.draw(screen)
+        door_group.draw(screen)
         gem_group.draw(screen)
-        
+        spikes_group.draw(screen)
+        water_group.draw(screen)
         game_over = player.update(game_over)
-        
+
         #if player died
         if game_over == -1:
+            pygame.mixer.music.pause()
             if restart_button.draw():
                 world_data =[]
                 world = reset_level(level)
                 game_over = 0
                 score = 0
+                pygame.mixer.music.unpause()
 
         #if player completes level
         if game_over == 1:
@@ -371,7 +402,7 @@ while run:
                 world = reset_level(level)
                 game_over = 0 
             else:
-                draw_text('YOU WIN!', font, white, (SCREEN_WIDTH //2)- 140, SCREEN_HEIGHT //2)
+                draw_text('YOU WIN!', font, black, 100, 140)
                 #restart game
                 if restart_button.draw():
                     level = 1
@@ -379,7 +410,6 @@ while run:
                     world = reset_level(level)
                     game_over = 0
                     score = 0
-
 
     #event handler
     for event in pygame.event.get():
